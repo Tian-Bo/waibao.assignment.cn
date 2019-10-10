6d7641559638b9bd6f9aaf635b2f1479d49d4adf
tianbo <543457309@qq.com>
2019-10-11 00:31:21 +0800

    测试

--- server/common.php
+++ server/common.php
@@ -55,8 +55,9 @@ class common
                             if ($value['goods_id'] == $goods_id) {
                                 // 数量加一
                                 $user_goods_list[$key]['number'] = $value['number'] + 1;
-                                $goods_json[$key]['goods_list'] = $user_goods_list;
+                                // $goods_json[$key]['goods_list'] = $user_goods_list;
                                 // 修改后将数据写入文件
+                                echo '111';die;
                                 $result = file_put_contents($file, json_encode($goods_json));
                                 if ($result) {
                                     return true;
