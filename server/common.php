07faec0e15adb2497c9930ee65f1db4a14fa96a1
tianbo <543457309@qq.com>
2019-10-11 00:49:38 +0800

    测试

--- server/common.php
+++ server/common.php
@@ -57,7 +57,8 @@ class common
                                 $user_goods_list[$key]['number'] = $value['number'] + 1;
                                 $goods_json[$key]['goods_list'] = $user_goods_list;
                                 // 修改后将数据写入文件
-                                $result = file_put_contents($file, json_encode($goods_json, JSON_UNESCAPED_UNICODE));
+                                echo $goods_json;die;
+                                $result = file_put_contents($file, json_encode($goods_json));
                                 if ($result) {
                                     echo '添加成功';die;
                                 }
