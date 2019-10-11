24ad4c304a8f742b96d8c5229510d8607df42445
tianbo <543457309@qq.com>
2019-10-12 02:46:30 +0800

    测试

--- server/createOrder.php
+++ server/createOrder.php
@@ -4,5 +4,5 @@ include 'common.php';
 header('Content-type:text/html;charset=utf-8');
 $commonObj = new common();
 $user_id = $_SESSION['user_info']['id'];
-// $card = isset($_GET['card']) ? $_GET['card'] : '';
+$card = isset($_GET['card']) ? $_GET['card'] : '';
 $result = $commonObj->createPay($user_id, $card);
