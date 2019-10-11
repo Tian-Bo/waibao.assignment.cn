56c3e9c8ba0a924ceb605e8d9c89d30c477c562c
tianbo <543457309@qq.com>
2019-10-12 00:43:41 +0800

    测试

--- server/addCart.php
+++ server/addCart.php
@@ -4,6 +4,7 @@ include 'common.php';
 header('Content-type:text/html;charset=utf-8');
 $commonObj = new common();
 $user_id = $_SESSION['user_info']['id'];
+$user_id = 1;
 // 获取商品id
 $goods_id = isset($_GET['goods_id']) ? $_GET['goods_id'] : '';
 $result = $commonObj->addCart($user_id, $goods_id);
